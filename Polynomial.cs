df4c37349ee210aeae5d1e3f9b91bc56ceb5a9d3
Rostyk <Rostyk@DESKTOP-0NMF6OL>
2021-10-05 21:12:42 +0300

    Добавьте файлы проекта.

---
+++ Polynomial.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Polynomial
+    {
+        private SortedDictionary<int, double> polynomials;
+
+
+        public Polynomial()
+        {
+            polynomials = new SortedDictionary<int, double>();
+        }
+
+        public int GetMaxOrder()
+        {
+            return polynomials.LastOrDefault().Key;
+        }
+
+
+        public void AddMonomials(int key, double value)
+        {
+            polynomials.Add(key, value);
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                return polynomials[index];
+            }
+            set
+            {
+                if(value != 0 && polynomials.ContainsKey(index))
+                {
+                    polynomials[index] = value;
+                }
+
+                if(value != 0 && !polynomials.ContainsKey(index))
+                {
+                    polynomials.Add(index, value);
+                }
+
+                if(value == 0 && polynomials.ContainsKey(index))
+                {
+                    polynomials.Remove(index);
+                }
+            }
+        }
+
+        public static Polynomial AddPolynomials(Polynomial p, Polynomial other)
+        {
+            int max = Math.Max(p.GetMaxOrder(), other.GetMaxOrder());
+            Polynomial newPolynomial = new Polynomial();
+
+            for (int i = 0; i <= max; ++i)
+            {
+                if (other.polynomials.ContainsKey(i) && p.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, p.polynomials[i] + other.polynomials[i]);
+                }
+                else if(other.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, other.polynomials[i]);
+                }
+                else if(p.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, p.polynomials[i]);
+                }
+            }
+            return newPolynomial;
+        }
+
+        public static Polynomial SubstractPolynomials(Polynomial p, Polynomial other)
+        {
+            int max = Math.Max(p.GetMaxOrder(), other.GetMaxOrder());
+            Polynomial newPolynomial = new Polynomial();
+
+            for (int i = 0; i <= max; ++i)
+            {
+                if (other.polynomials.ContainsKey(i) && p.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, p.polynomials[i] - other.polynomials[i]);
+                }
+                else if (other.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, other.polynomials[i]);
+                }
+                else if (p.polynomials.ContainsKey(i))
+                {
+                    newPolynomial.AddMonomials(i, p.polynomials[i]);
+                }
+            }
+            return newPolynomial;
+        }
+
+        public static Polynomial MultiplyPolynomial(Polynomial p, Polynomial other)
+        {
+            int n = Math.Max(p.polynomials.Count, other.polynomials.Count);
+
+            Polynomial newPolynomial = new Polynomial();
+            double value = 0.0;
+            int key = 0;
+            for (int i = 0; i < p.polynomials.Count; ++i)
+            {
+                for (int j = 0; j < other.polynomials.Count; ++j)
+                {
+                    value = (p.polynomials.ElementAt(i).Value) * (other.polynomials.ElementAt(j).Value);
+                    key = (p.polynomials.ElementAt(i).Key) + (other.polynomials.ElementAt(j).Key);
+
+                    if (newPolynomial.polynomials.ContainsKey(key))
+                    {
+                        newPolynomial[key] += value;
+                    }
+                    else
+                    {
+                        newPolynomial.AddMonomials(key, value);
+                    }
+                }
+                
+              
+            }
+            return newPolynomial;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var coef in polynomials)
+            {
+                sb.Append(string.Format("{0}*x^{1} + ", coef.Value, coef.Key));
+            }
+             
+            sb.Append("\n");
+            return sb.ToString(); 
+        }
+
+        private KeyValuePair<int, double> ParseMonomial(char sign, string monomial)
+        {
+            int power = 0;
+            double coef = 0.0;
+            for (int i = 0; i < monomial.Length; ++i)
+            {
+                if (monomial[i] == 'x')
+                {
+                    if(i + 2 >= monomial.Length)
+                    {
+                        throw new IndexOutOfRangeException("");
+                    }
+                    if(monomial[i+1] != '^')
+                    {
+                        throw new FormatException("Incorrect format of monomial. Expected '^'");
+                    }
+                    
+                    if(!char.IsDigit(monomial[i + 2]))
+                    {
+                        throw new Exception("Expected power of the monomial.");
+                    }
+                    power = Convert.ToInt32(monomial.Substring(i + 2, monomial.Length - i - 2));  
+                   
+                    if(i!=0 && !double.TryParse(monomial.Substring(0, i), out coef))
+                    {
+                        throw new FormatException("Can't parse the value of coefficient");
+                    }
+                    break;
+                }
+               
+            }
+            if(sign == '-')
+            {
+                coef = -coef;
+            }
+            return new KeyValuePair<int, double>(power, coef);
+        }
+
+        public void Parse(string polynomial)
+        {
+            int currentIndex = 0;
+            char sign = '+';
+            while (currentIndex < polynomial.Length) 
+            {
+                string monomial = "";
+
+                for (int i = currentIndex; i < polynomial.Length; ++i)
+                {
+                    if (polynomial[i] == ' ')
+                    {
+                        continue;
+                    }
+                    if (polynomial[i] == '+' || polynomial[i] == '-')
+                    {
+                        if(i == currentIndex)
+                        {
+                            sign = polynomial[i];
+                        }
+                        else
+                        {
+                         
+                            currentIndex = i;
+                            break;
+                        }
+                     
+                    }
+
+                    else
+                    {
+                        monomial += polynomial[i];
+                        if(i+1 >= polynomial.Length)
+                        {
+                            currentIndex = i + 1;
+                        }
+                    }
+                }
+
+                var m = ParseMonomial(sign, monomial);      
+                this.polynomials.Add(m.Key, m.Value);
+
+            }
+
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial p = new Polynomial();
+            p.AddMonomials(0, 3);
+            p.AddMonomials(1, 2);
+            p.AddMonomials(2, -5);
+            p.AddMonomials(3, 8);
+            Console.WriteLine(p.ToString());
+
+            Polynomial p2 = new Polynomial();
+            p2.AddMonomials(0, 1);
+            p2.AddMonomials(1, 2);
+            p2.AddMonomials(2, 3);
+            p2.AddMonomials(5, 4);
+            Console.WriteLine(p2.ToString());
+
+            var other = Polynomial.AddPolynomials(p2, p);
+            Console.WriteLine(other.ToString());
+            var other2 = Polynomial.SubstractPolynomials(p2, p);
+            Console.WriteLine(other2.ToString());
+
+            var multiply = Polynomial.MultiplyPolynomial(p, p2);
+            Console.WriteLine(multiply.ToString());
+
+            Polynomial p3 = new Polynomial();
+            p3.Parse("8x^0 + 4x^2");
+            Console.WriteLine(p3.ToString());
+            Console.ReadLine();
+        }
+    }
+}
